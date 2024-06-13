57221192dfa5cce46928d1bd2a700f30509e9c73
Wojciech Jozwik <wj.telefon@gmail.com>
2024-06-13 22:37:11 +0200

    Fix server

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
-const app = express();
 
+const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
-const testimonialsRoutes = require('./routes/testimonials.routes');
 
-// app.use(cors());
+const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//     methods: 'GET,POST,PUT,DELETE',
-//   })
-// );
-
+  
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,POST,PUT,DELETE',
+  })
+);
+app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
-app.use('/api', testimonialsRoutes);
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/client/build')));
@@ -35,4 +34,3 @@ app.use((req, res) => {
 app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
-
