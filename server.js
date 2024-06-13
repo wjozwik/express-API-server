3467ac23bbe424ef4b28e80013ecdde23889e4b9
Wojciech Jozwik <wj.telefon@gmail.com>
2024-06-13 22:24:05 +0200

    Update server.js and add build app

--- server.js
+++ server.js
@@ -1,31 +1,38 @@
 const express = require('express');
-const cors = require('cors');
+const path = require('path');
 const app = express();
 
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 const testimonialsRoutes = require('./routes/testimonials.routes');
 
-app.use(cors());
+// app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,POST,PUT,DELETE',
-  })
-);
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: 'GET,POST,PUT,DELETE',
+//   })
+// );
 
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 app.use('/api', testimonialsRoutes);
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
 
